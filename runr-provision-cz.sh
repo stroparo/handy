38d67dcf9a71422572a6f2ee5b8d8133130f3df8
Cristian Stroparo <stroparo@outlook.com>
2021-10-05 23:05:06 -0300

    Add curl installation for poor Debian/Ubuntu distros

--- runr-provision-cz.sh
+++ runr-provision-cz.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 {
+which curl || (sudo apt-get install curl >/dev/null 2>&1)
 bash -c "$(curl -LSf "https://bitbucket.org/stroparo/runr/raw/master/entry.sh" \
   || curl -LSf "https://raw.githubusercontent.com/stroparo/runr/master/entry.sh")" \
   entry.sh provision-cz
