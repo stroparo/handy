d1993d945e99d5dcffb349fa3be77ca736ee437e
Cristian Stroparo <stroparo@outlook.com>
2022-04-13 01:41:37 -0300

    Streamline runr-*.sh provisioning scripts

--- runr-provision-cz.sh
+++ runr-provision-cz.sh
@@ -1,24 +1,16 @@
 #!/bin/bash
 {
 RECIPE=provision-cz
+: ${DEV:=${HOME}/workspace}
 which curl || (sudo apt-get install curl >/dev/null 2>&1) || (sudo pacman -S curl >/dev/null 2>&1)
-if [ -d "${DEV}/runr" ] ; then
-  # Setup from local files:
-  if [ -d "${DEV}/dotfiles" ] ; then
-    (
-      export RUNR_ASSETS_REPOS="${DEV}/dotfiles"
-      export RUNR_ASSETS_REPOS_FALLBACKS=""
-      bash -c "$(cat "${DEV}/runr/entry.sh")" entry.sh "${RECIPE}"
-    )
-  else
-    bash -c "$(cat "${DEV}/runr/entry.sh")" entry.sh "${RECIPE}"
-  fi
-else
-  # Setup from remote files:
-  bash -c "$(curl -LSf "https://bitbucket.org/stroparo/runr/raw/master/entry.sh" \
-      || curl -LSf "https://raw.githubusercontent.com/stroparo/runr/master/entry.sh")" \
-      entry.sh "${RECIPE}"
+if [ -d "${DEV}/dotfiles" ] ; then
+  export RUNR_ASSETS_REPOS="${DEV}/dotfiles"
+  export RUNR_ASSETS_REPOS_FALLBACKS=""
 fi
+bash -c "$(cat "${DEV}/runr/entry.sh" 2>/dev/null \
+    || curl -LSf "https://bitbucket.org/stroparo/runr/raw/master/entry.sh" \
+    || curl -LSf "https://raw.githubusercontent.com/stroparo/runr/master/entry.sh")" \
+    entry.sh "${RECIPE}"
 . "${ZDRA_HOME:-$HOME/.zdra}/zdra.sh"
 . ~/.aliases-cs
 }
